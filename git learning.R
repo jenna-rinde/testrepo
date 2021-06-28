7161ed5bf96c79347023a81ae5475851d2adcca3
jenna-rinde <jenna.rinde@wildlife.ca.gov>
2021-06-28 14:16:39 -0700

    Create git learning.R

---
+++ git learning.R
@@ -0,0 +1,9 @@
+#install the package 'usethis'
+library(usethis)
+
+usethis::use_git_config(user.name = "Jenna Rinde", user.email = "jennarinde@outlook.com")
+usethis::git_sitrep()
+library(usethis)
+git_vaccinate()
+
+  
